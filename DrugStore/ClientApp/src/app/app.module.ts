5faa6530ee35c7b476d9b68c5866f0e111ae6c99
Anton Orekhovskiy <OrekhovskiyA@qmobile.ru>
2020-02-03 18:18:58 +0300

    medicine and login components added

--- DrugStore/ClientApp/src/app/app.module.ts
+++ DrugStore/ClientApp/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { MedicinesComponent } from './medicines/medicines.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MedicinesComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
