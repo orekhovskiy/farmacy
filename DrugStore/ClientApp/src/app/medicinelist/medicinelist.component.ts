04905bfa0fb1e97962ce25c71b248dda888b2375
Anton Orekhovskiy <OrekhovskiyA@qmobile.ru>
2020-02-06 17:28:38 +0300

    Medicine list converted into table!

--- DrugStore/ClientApp/src/app/medicinelist/medicinelist.component.ts
+++ DrugStore/ClientApp/src/app/medicinelist/medicinelist.component.ts
@@ -17,8 +17,28 @@ export class MedicinelistComponent implements OnInit {
   ngOnInit() {
     if ($(document).height() <= $(window).height())
       $("#footer").addClass("fixed-bottom");
+    this.aspirine = {
+      name: 'Аспирин',
+      producer:'Bayer',
+      type: ["Анальгетик", "Жаропонижающее"],
+      form: "Таблетки",
+      available: 45,
+      composition: ["Ацетилсалициловая кислота"],
+      shelfTime:18
+    }
+    this.medicines = [this.aspirine, this.aspirine, this.aspirine, this.aspirine, this.aspirine];
   }
-
+  
+  //down chevrone - &#8964;
+  //right triangle bracket - $gt;
+  changeArrow(id: string) {
+    
+    var obj = document.getElementById(id);
+    if (obj.innerHTML === "&gt;" )
+      obj.innerHTML = "&#x2304;";
+    else
+      obj.innerHTML = ">";
+  }
 }
 
 interface Medicine {
@@ -28,5 +48,5 @@ interface Medicine {
   form: string;
   available: number;
   composition: string[];
-  shelftime: number;
+  shelfTime: number;
 }
